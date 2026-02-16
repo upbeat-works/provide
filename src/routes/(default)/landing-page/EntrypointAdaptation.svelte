be218ac230e8416d6076cbea7aa7a1a220fbaaf4
Rodrigo Solís <sorodrigo@users.noreply.github.com>
2026-02-17 00:57:36 +0100

    Revert "Refactor landing page into section components"

---
+++ src/routes/(default)/landing-page/EntrypointAdaptation.svelte
@@ -0,0 +1,37 @@
+<script>
+  import LinkArrow from '$lib/helper/icons/LinkArrow.svelte';
+  import { PATH_ADAPTATION } from '$config';
+  export let title;
+  export let caseStudies = [];
+</script>
+
+<div class="grid grid-rows-[1fr_auto_auto] bg-surface-weaker p-4 sm:p-10 md:p-12 {$$props.class}">
+  <div class="flex flex-col gap-y-2 my-4">
+    <span class="uppercase text-left font-bold text-xl tracking-wide text-current">{title}</span>
+    <div class="block text-left text-base sm:text-lg md:leading-normal text-black">
+      <slot name="intro" />
+    </div>
+  </div>
+  <div>
+    <a class="inline-flex items-center gap-x-4 hover:bg-surface-weakest py-3 px-2 text-theme-base transition-colors" href={`/${PATH_ADAPTATION}`}>
+      <div>
+        <h4 class="font-bold text-theme-base text-base sm:text-lg md:leading-normal">See the case study overview</h4>
+        <span class="text-text-weaker text-sm">Learn how to use climate data for overshoot risk informed adaptation.</span>
+      </div>
+      <LinkArrow />
+    </a>
+  </div>
+  {#if caseStudies.length}
+    <span class="text-contour-weaker text-sm mt-6">Case studies:</span>
+    <ul class="grid sm:grid-cols-2 md:grid-cols-4 gap-4 my-2">
+      {#each caseStudies as { city, abstract }}
+        <li>
+          <a href="/{PATH_ADAPTATION}/{city.uid}" class="block hover:bg-surface-weakest py-3 px-2 transition-colors">
+            <h4 class="font-bold text-theme-base">{city.label}</h4>
+            <p class="text-text-weaker text-sm">{abstract}</p></a
+          >
+        </li>
+      {/each}
+    </ul>
+  {/if}
+</div>
---
+++ src/routes/(default)/landing-page/EntrypointExplore.svelte
@@ -0,0 +1,41 @@
+<script>
+  import { PATH_EXPLORE } from '$config';
+  export let path;
+  export let title;
+  export let audience = [];
+</script>
+
+<a href={`/${PATH_EXPLORE}/${path}`} class="hover:bg-surface-weakest hover:text-theme-base transition-colors grid grid-rows-[1fr_auto_auto] bg-surface-weaker p-4 sm:p-10 md:p-12 {$$props.class}">
+  <div class="flex flex-col gap-y-2 my-4">
+    <span class="uppercase text-center font-bold text-xl tracking-wide text-current">{title}</span>
+    <div class="block text-center text-base sm:text-lg md:leading-normal text-black">
+      <slot name="intro" />
+    </div>
+  </div>
+  {#if audience.length}
+    <span class="text-contour-weaker text-sm mt-6">Most suitable for:</span>
+    <ul class="my-2 flex gap-y-2 flex-col text-base">
+      {#each audience as label}
+        <li class="flex items-center gap-x-2">
+          <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M3.17688 7.73688C2.77774 7.36946 2.14852 7.38691 1.77148 7.77585C1.39443 8.1648 1.41234 8.77795 1.81147 9.14537L5.71148 12.7355C6.12186 13.1132 6.77232 13.0826 7.1434 12.668L15.255 3.6056C15.6159 3.20239 15.5731 2.59041 15.1593 2.23871C14.7455 1.88702 14.1175 1.92878 13.7566 2.332L6.32492 10.6347L3.17688 7.73688Z"
+              fill="#316F91"
+            />
+          </svg>
+          <span class=" text-black">{label}</span>
+        </li>
+      {/each}
+    </ul>
+  {/if}
+  {#if $$slots.image}
+    <figure class="p-0 md:p-0 py-6 sm:px-6 sm:pt-4 md:pt-8 pb-4">
+      <slot name="image" />
+    </figure>
+  {/if}
+  {#if $$slots.footer}
+    <footer class="text-xs mt-4 text-black">
+      <slot name="footer" />
+    </footer>
+  {/if}
+</a>
